dc24b6e233f3fa4192774908db685086927d6a17
hashish <Mhashish@smart-medicalservices.com>
2018-01-18 19:11:53 +0200

    model serializer

--- notification/serializers.py
+++ notification/serializers.py
@@ -7,15 +7,12 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'password','email', 'first_name', 'last_name', 'is_superuser', 'date_joined')
 
-class NotificationSerializer(serializers.Serializer):
-    user_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), source='user',
-                                                          allow_null=False, required=True)
-    message = serializers.CharField(max_length=1023)
-    created_Date = serializers.DateTimeField()
-    is_read = serializers.BooleanField(default=False)
+class NotificationSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True, required=False)
     class Meta:
         model = Notification
-        fields = ('id'
+        fields = ('id',
+                  'user',
                   'user_id',
                   'is_read',
                   'created_Date'
